3cb27c9f6cc56e989bbdf07b07cff348b5e4e1b1
Andrei <shehs6767@mail.ru>
2021-12-12 22:35:15 +0300

    add

--- js/main.js
+++ js/main.js
@@ -34,22 +34,11 @@ const app = new Vue({
             .then(data => {
                 for (let el of data) {
                     this.products.push(el)
-
+                    this.filtered.push(el)
                 }
             })
     }
 })
-
-
-
-
-
-
-
-
-
-
-
 
 
 
